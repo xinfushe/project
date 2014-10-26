81a5f452139e5f78aee817a8cfbaf8740ce54491
caoyilun <caoyilun@sjtu.edu.cn>
2014-10-26 18:35:37 +0800

    sh files

---
+++ sh/opencv-config.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo su
+cd /etc
+echo "/usr/local/lib" >> ld.so.conf
+ldconfig
+
+然后编辑/etc/bash.bashrc
+
+加入
+
+PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
+export PKG_CONFIG_PATH
+
+sudo source /etc/bash.bashrc
